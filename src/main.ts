2d0bd7515f6e93ea97df163cbdee7e8fde3e6926
Coverage <coverage@bot.com>
2021-02-03 11:23:52 +0000

    try and use s3

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {promisify} from 'util'
-import childProcess from 'child_process'
-import {getSummary} from './getCoverage'
 import fs from 'fs'
 import table from 'markdown-table'
 
 import {S3} from 'aws-sdk'
+import {getSummary} from './getCoverage'
 
 const s3 = new S3()
-
-const exec = promisify(childProcess.exec)
 
 const getCoverageFile = () => {
   try {
@@ -39,71 +35,90 @@ export const generateTable = (base: any, compare: any) => {
   ])
 }
 
+const uploadFile = (branch: string, filePath: string) => {
+  const readStream = fs.createReadStream(filePath)
+
+  return new Promise<S3.ManagedUpload.SendData>((resolve, reject) => {
+    s3.upload(
+      {
+        Bucket: 'cko-prism-frontend',
+        Key: `checks/${branch}/coverage-summary.json`,
+        Body: readStream
+      },
+      (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      }
+    )
+  })
+}
+
+const download = (branch: string) => {
+  return new Promise<any>((resolve, reject) => {
+    s3.getObject(
+      {
+        Bucket: 'cko-prism-frontend',
+        Key: `checks/${branch}/coverage-summary.json`
+      },
+      (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          if (data.Body) {
+            resolve(JSON.parse(data.Body.toString()))
+          }
+        }
+      }
+    )
+  })
+}
+
 async function run(): Promise<void> {
   try {
     //get current
     const compareCoverage = getCoverageFile()
 
-    const readStream = fs.createReadStream(
+    await uploadFile(
+      process.env.GITHUB_HEAD_REF!,
       'coverage-compare/coverage-summary.json'
-    )
-
-    s3.upload(
-      {
-        Bucket: 'cko-prism-frontend',
-        Key: 'checks/coverage-summary.json',
-        Body: readStream
-      },
-      (err, data) => {
-        if (err) {
-          console.log('Error', err)
-        } else {
-          console.log('Success', data)
-        }
-      }
     )
 
     //get base (from S3)
 
-    // s3.getObject({Bucket: 'cko-prism-frontend', Key: 'checks/coverage-summary.json'}, (err, data) => {
-    //   if (err) {
-    //     console.log('Error', err)
-    //   } else {
-    //     console.log('Success', data)
-    //   }
-    // })
+    const baseCoverage = await download(process.env.GITHUB_BASE_REF!)
 
-    // const baseCoverage = getCoverageFile()
+    const githubToken = core.getInput('githubToken', {required: true})
 
-    console.log(fs.readdirSync('./coverage-compare'))
+    if (baseCoverage) {
+      const table = generateTable(
+        getSummary(baseCoverage),
+        getSummary(compareCoverage)
+      )
 
-    // const githubToken = core.getInput('githubToken', {required: true})
+      const octokit = new github.GitHub(githubToken)
 
-    // if (baseCoverage) {
-    //   const table = generateTable(
-    //     getSummary(baseCoverage),
-    //     getSummary(compareCoverage)
-    //   )
+      const context = github.context
 
-    //   const octokit = new github.GitHub(githubToken)
+      const pullRequest = context.payload.pull_request
 
-    //   const context = github.context
+      if (pullRequest == null) {
+        core.setFailed('No pull request found.')
+        return
+      }
 
-    //   const pullRequest = context.payload.pull_request
+      const pull_request_number = pullRequest.number
 
-    //   if (pullRequest == null) {
-    //     core.setFailed('No pull request found.')
-    //     return
-    //   }
-
-    //   const pull_request_number = pullRequest.number
-
-    //   await octokit.issues.createComment({
-    //     ...context.repo,
-    //     issue_number: pull_request_number,
-    //     body: table
-    //   })
-    // }
+      await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request_number,
+        body: table
+      })
+    } else {
+      console.log('no base coverage found')
+    }
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
